3b7767a5f6dd906fd840119ea69eecaea0f33f87
jojogreen <e.j.kaiser17@gmail.com>
2015-06-22 21:57:25 -0500

    Added more user functionality including datepicker and more outputs.

--- Orbital Mechanix Suite/Form1.Designer.cs
+++ Orbital Mechanix Suite/Form1.Designer.cs
@@ -29,39 +29,27 @@
         private void InitializeComponent()
         {
             this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.day_numeric = new System.Windows.Forms.NumericUpDown();
-            this.output_label = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.day_numeric)).BeginInit();
+            this.datePick = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.outputBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(63, 45);
+            this.comboBox1.Location = new System.Drawing.Point(16, 113);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 0;
             // 
-            // day_numeric
-            // 
-            this.day_numeric.Location = new System.Drawing.Point(210, 46);
-            this.day_numeric.Name = "day_numeric";
-            this.day_numeric.Size = new System.Drawing.Size(120, 20);
-            this.day_numeric.TabIndex = 1;
-            // 
-            // output_label
-            // 
-            this.output_label.AutoSize = true;
-            this.output_label.Location = new System.Drawing.Point(473, 137);
-            this.output_label.Name = "output_label";
-            this.output_label.Size = new System.Drawing.Size(35, 13);
-            this.output_label.TabIndex = 2;
-            this.output_label.Text = "label1";
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(79, 95);
+            this.button1.Location = new System.Drawing.Point(504, 65);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 3;
@@ -69,20 +57,99 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // datePick
+            // 
+            this.datePick.Location = new System.Drawing.Point(143, 114);
+            this.datePick.Name = "datePick";
+            this.datePick.Size = new System.Drawing.Size(200, 20);
+            this.datePick.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(511, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Answer";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "True Anomaly (degrees)",
+            "Radius From Orbitng Body (m)",
+            "Semi-Major Axis (m)",
+            "Eccentricity",
+            "Inclination (degrees)",
+            "Longitude of Ascending Node (degrees)",
+            "Argument of Periapse (degrees)",
+            "Mass (Kg)"});
+            this.checkedListBox1.Location = new System.Drawing.Point(34, 195);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(215, 124);
+            this.checkedListBox1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Select a Planet";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(274, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Select desired Orbit Components";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(182, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Select Date";
+            // 
+            // outputBox
+            // 
+            this.outputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputBox.Location = new System.Drawing.Point(371, 119);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ReadOnly = true;
+            this.outputBox.Size = new System.Drawing.Size(378, 274);
+            this.outputBox.TabIndex = 10;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(779, 405);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.datePick);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.output_label);
-            this.Controls.Add(this.day_numeric);
             this.Controls.Add(this.comboBox1);
             this.ImeMode = System.Windows.Forms.ImeMode.Disable;
             this.Name = "Form1";
             this.Text = "Orbital Mechanix Suite";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.day_numeric)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,9 +158,14 @@
         #endregion
 
         private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.NumericUpDown day_numeric;
-        private System.Windows.Forms.Label output_label;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker datePick;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox outputBox;
     }
 }
 
--- Orbital Mechanix Suite/Form1.cs
+++ Orbital Mechanix Suite/Form1.cs
@@ -13,6 +13,8 @@ namespace Orbital_Mechanix_Suite
 
     public partial class Form1 : Form
     {
+        public int year, month, day;
+        double daysFromJ2000;
         public List<Planet> PlanetList = new List<Planet>();
         private static double AU2m = 149597870700;
         public Planet Mercury = new Planet("Mercury", 1.976193311288083E-01, 7.013873298083790, 5.885642355456797E10, 4.812376548400915E1, 2.581669078701001E1, 1.789289550796730E2, 3.302E23);
@@ -20,6 +22,19 @@ namespace Orbital_Mechanix_Suite
         public Planet Earth = new Planet("Earth", 1.539024710796432E-02, 1.179024940726686E-02, 1.489476741134844E11, -1.059863596387948E+01, 6.373464993332024E+01, 2.570514546707050E+01, 5.97219E24);
         //public Planet Mars = new Planet();
 
+        private void getDate()
+        {
+            day = datePick.Value.Day;
+            month = datePick.Value.Month;
+            year = datePick.Value.Year;
+
+            int temp1 = (year+(month+9)/12)/4;
+            int temp2 = 275*month/9;
+
+            daysFromJ2000 = (double) 367 * year - 7 * temp1 + temp2 + day - 730531.5 +.5;
+            Console.Write(daysFromJ2000);
+            
+        }
 
         public Form1()
         {
@@ -56,16 +71,71 @@ namespace Orbital_Mechanix_Suite
         {
             double TA = 0;
             string selectedPlanet = comboBox1.Text;
-            float day = (float)day_numeric.Value;
+           // float day = (float)day_numeric.Value;
+            
+            getDate();
             foreach (Planet plan1 in PlanetList)
-            {
-                if (selectedPlanet == plan1.name)
                 {
-                    TA = plan1.True_anomaly(day);
-                    break;
+                    if (selectedPlanet == plan1.name)
+                    {
+                        outputBox.Text = "";
+                        outputBox.AppendText("Computed Results for " + plan1.name + "\r\n");
+                        outputBox.AppendText("Date for the Computation is " + datePick.Value.ToLongDateString() + "\r\n\r\n");
+                        outputBox.AppendText("%----------------------------------------------------------%\r\n\r\n");
+                        foreach (int item in checkedListBox1.CheckedIndices)
+                        {
+                            switch (item)
+                            {
+                                case 0:
+                                    TA = plan1.True_anomaly(daysFromJ2000);
+                                    outputBox.AppendText("True Anomaly (deg) = " + TA + "\r\n");
+                                    break;
+                                case 2:
+                                    double Radius = plan1.Radius(daysFromJ2000);
+                                    outputBox.AppendText("Radius from Orbiting Body (m) = " + Radius + "\r\n");
+                                    break;
+                                case 3:
+                                    double SemiMajor = plan1.semi_Major_Axis;
+                                    outputBox.AppendText("Semi-Major Axis (m) = " + SemiMajor + "\r\n");
+                                    break;
+                                case 4:
+                                    double Eccentr = plan1.eccentricity;
+                                    outputBox.AppendText("Eccentricity = " + Eccentr + "\r\n");
+                                    break;
+                                case 5:
+                                    double inc = plan1.inclination;
+                                    outputBox.AppendText("Inclination (deg) = " + inc + "\r\n");
+                                    break;
+                                case 6:
+                                    double longAscNode = plan1.Long_asc_node;
+                                    outputBox.AppendText("Longitude of Ascending Node (deg)= " + longAscNode + "\r\n");
+                                    break;
+                                case 7:
+                                    double ArgPer = plan1.arg_periapse;
+                                    outputBox.AppendText("Argument of Periapse (deg) = " + ArgPer + "\r\n");
+                                    break;
+                                case 8:
+                                    double Mass = plan1.mass_Planet;
+                                    outputBox.AppendText("Mass (Kg)" + Mass + "\r\n");
+                                    break;
+                                default:
+                                    TA = plan1.True_anomaly(daysFromJ2000);
+                                    Radius = plan1.Radius(daysFromJ2000);
+                                    break;
+                            }
                 }
-            }
-            output_label.Text = TA.ToString("#0.00");
+
+                    }
+                }
+
+
+
+            
+        }
+
+        private void output_label_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
